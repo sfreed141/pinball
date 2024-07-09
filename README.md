07a62617d4c2799c832f0a7c751c43b3121941bf
Sam Freed <sfreed141@gmail.com>
2024-07-09 15:44:32 -0700

    separate left + right paddles

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Prototype pinball game made with Godot 4.2
 
 # credits
+- SimpleShapes 2D assets from GiantLightStudios
--- project/game/paddle.gd
+++ project/game/paddle.gd
@@ -4,16 +4,19 @@ extends AnimatableBody2D
 var _tween
 const SLAP_ANGLE = 30
 
+var _is_left
+
+func _ready():
+	_is_left = cos(_base_angle) > 0
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("slap") and not (_tween and _tween.is_running()):
-		var target_angle = _base_angle
+	var target_angle = _base_angle
+	var slap = Input.is_action_pressed("slap") or (_is_left and Input.is_action_pressed("left")) or (not _is_left and Input.is_action_pressed("right"))
+	if slap:
 		if cos(_base_angle) < 0:
 			target_angle += SLAP_ANGLE
 		else:
 			target_angle -= SLAP_ANGLE
-		_tween = create_tween()	\
-			.set_ease(Tween.EASE_IN_OUT)	\
-			.set_trans(Tween.TRANS_BOUNCE)
-		_tween.tween_property(self, "rotation_degrees", target_angle, 0.05)
-		_tween.tween_property(self, "rotation_degrees", _base_angle, 0.15)
+	
+	rotation = lerp_angle(rotation, deg_to_rad(target_angle), 0.5)
+	
