8149e644103967c8277abe6e9ce7635c5b9d0b69
John Nordberg <squidinkdesign@gmail.com>
2024-07-09 16:22:42 -0700

    changed layout of board, added a plunger area, changed plunger variables

--- project/game/ball.gd
+++ project/game/ball.gd
@@ -1,7 +1,18 @@
 extends RigidBody2D
 
+var launched = false
+@export var launchSpeed: float = 2000
+
 signal bonk()
+
+func _process(delta):
+	if (Input.is_action_pressed("slap")) and launched == false:
+		launched = true
+		var upward_impulse = Vector2(0, -launchSpeed)
+		apply_central_impulse(upward_impulse)
+		print("Launched")
 
 func _on_body_entered(body):
 	if body.is_in_group(&"paddle"):
 		bonk.emit()
+
